fb8b9f08d0f614ea136aa8a4c26ca58e6674584b
Alexei.Svitkine <Alexei.Svitkine@1dfe28b9-0c07-232b-11d8-143845564945>
2008-01-28 00:11:18 +0000

    test write() also

--- src/jhexedit/bdoc/test/TestBinaryDocument.java
+++ src/jhexedit/bdoc/test/TestBinaryDocument.java
@@ -20,4 +20,20 @@ public class TestBinaryDocument extends TestCase {
 			assertEquals(buf[i], buf2[i]);
 		}
 	}
+
+	public void testWritingLargeAmountOfData() throws Exception {
+		BinaryDocument doc = new BinaryDocument();
+		assertEquals(0, doc.length());
+		byte[] buf = new byte[512];
+		for (int i = 0; i < buf.length; i++) {
+			buf[i] = (byte) i;
+		}
+		doc.write(doc.createOffset(0), buf);
+		assertEquals(buf.length, doc.length());
+		byte[] buf2 = new byte[buf.length];
+		assertEquals(buf.length, doc.read(doc.createOffset(0), buf2));
+		for (int i = 0; i < buf.length; i++) {
+			assertEquals(buf[i], buf2[i]);
+		}
+	}
 }
